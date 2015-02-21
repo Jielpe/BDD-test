51280b183e3f5bbaca82c68b032bc0479f32a3f1
Jielpe <jielpe99@gmail.com>
2015-02-21 12:25:45 +0100

    Fichier commandes MySQL
    Pour aller plus vite?

---
+++ Societe.sql
@@ -0,0 +1,2 @@
+INSERT INTO Societe (nom,contexte,situation_eco,secteur,chiffres_clefs,part_marches,nb_employes,coeur_metier,valeurs,culture,fournisseurs_sous_traitants,concurrents,implantation,commentaires)
+VALUES ('Cofely','Transition énergétique','B','Industriel','CA=14,7 Md','20%','90000','Services en efficacité énergétique','diversité','expertise et proximité',NULL,'Dalkia','FR',NULL)
